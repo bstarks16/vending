195398e3bf7889bbc78e4d3fe7ef898c779582ce
bstarks16 <59940602+bstarks16@users.noreply.github.com>
2022-03-03 11:23:16 -0600

    Update dns_changer.sh

--- dns_changer.sh
+++ dns_changer.sh
@@ -8,5 +8,5 @@ echo Please enter your DNS address
 
 read dns
 
-echo  nameserver $dns >> /etc/resolvconf/resolv.conf.d/head
+sudo echo  nameserver $dns >> /etc/resolvconf/resolv.conf.d/head
 sudo systemctl restart resolvconf.service
