f4f4ec9b7e7967c8cb451bf52f828cd975e24fe5
bstarks16 <59940602+bstarks16@users.noreply.github.com>
2022-03-03 09:30:27 -0600

    Add files via upload

---
+++ update_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+echo "Vending Machine Upgrade Script" 
+echo "written by Brian Starks & Jessica Davis - CHI L2"
+read -p "Press enter to continue."
+sudo apt install sshpass
+sudo pkill UI
+cd /opt/barracuda
+s
+clear
+echo "Please enter the password to complete setup: "
+sudo sshpass scp signifi@10.248.48.14:~/Downloads/UI/UI .
+sudo chmod 755 UI
+sudo reboot
