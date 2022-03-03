468a837e1e1088d749b19cf4c4fece17711f6fc7
bstarks16 <59940602+bstarks16@users.noreply.github.com>
2022-03-03 09:27:33 -0600

    Update dns_changer.sh

--- dns_changer.sh
+++ dns_changer.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo "Vening Machine DNS Updater" 
+echo "written by Brian Starks & Jessica Davis - CHI L2"
+read -p "Press enter to continue."
+
 echo Please enter your DNS address
 
 read dns
 
 echo -e "nameserver $dns" >> /etc/resolvconf/resolv.conf.d/head
-
