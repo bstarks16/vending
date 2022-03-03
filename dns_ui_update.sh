2e3694c4773d5373b7f62281f9c809a7a3d9e0d6
bstarks16 <59940602+bstarks16@users.noreply.github.com>
2022-03-03 10:34:27 -0600

    Update dns_ui_update.sh

--- dns_ui_update.sh
+++ dns_ui_update.sh
@@ -5,6 +5,6 @@ read -p "Press enter to continue."
 sudo apt install git
 git clone https://github.com/bstarks16/vending.git
 cd vending/
-sh dns_changer.sh 
+dns_changer.sh 
 read -p "DNS has been successfully updated. Press enter to continue updating the UI"
 sudo sh update_script.sh
