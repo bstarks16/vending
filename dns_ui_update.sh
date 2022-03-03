5630ee6e38625674b86168a2590ab1fa5943b43b
bstarks16 <59940602+bstarks16@users.noreply.github.com>
2022-03-03 10:36:26 -0600

    Update dns_ui_update.sh

--- dns_ui_update.sh
+++ dns_ui_update.sh
@@ -4,7 +4,7 @@ echo "written by Brian Starks & Jessica Davis - CHI L2"
 read -p "Press enter to continue."
 sudo apt install git
 git clone https://github.com/bstarks16/vending.git
-cd vending/
+cd vending
 dns_changer.sh 
 read -p "DNS has been successfully updated. Press enter to continue updating the UI"
 sudo sh update_script.sh
