c3b5c4db0539edc4c66144a39dcce43127d8e585
bstarks16 <59940602+bstarks16@users.noreply.github.com>
2022-03-03 15:19:45 -0600

    Update dns_ui_updater.sh

--- dns_ui_updater.sh
+++ dns_ui_updater.sh
@@ -3,14 +3,13 @@ clear
 echo "Vending Machine DNS and UI Updater" 
 echo "written by Brian Starks & Jessica Davis - CHI L2"
 read -p "Press enter to continue."
-sudo apt install git
-git clone https://github.com/bstarks16/vending.git
-clear
 scp signifi@10.248.48.14:~/dns_changer.sh .
-cd vending/
 clear
-sudo bash ~/dns_changer.sh 
+bash sudo ~/dns_changer.sh 
 clear
 read -p "DNS Update Complete. Press Enter to Continue"
+sudo apt install git
+git clone https://github.com/bstarks16/vending.git
+cd vending/
 clear
 bash update_script.sh
